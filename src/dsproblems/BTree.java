162c1fabdc0ef25c764435fd31e25c357aaf4bcc
jaychapani <jaychapani@yahoo.com>
2018-07-30 11:56:10 -0700

    update

---
+++ src/dsproblems/BTree.java
@@ -0,0 +1,197 @@
+package dsproblems;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
+class Node {
+	int data;
+	Node left, right;
+
+	public Node(int item) {
+		data = item;
+		left = right = null;
+	}
+}
+
+public class BTree {
+
+	public static void main(String[] args) {
+
+		Node root = new Node(1);
+
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+
+		// int a[] = new int[] { 2, 3, 4, 5, 6, 7, 1, 8 };
+
+		// Node root1 = null;
+
+		// for (int i = 0; i < a.length; i++) {
+		// root1 = insertBSTRec(root1, a[i]);
+		// }
+
+		// printInorder(root1);
+
+		printCousin(root, root.left.left);
+
+		// System.out.println(isMirror(root, root));
+
+		// printLevelOrder(root);
+
+		// System.out.println(height(root));
+
+	}
+
+	private static void printCousin(Node root, Node n) {
+
+		if (root == null) {
+			System.out.println("No Cousins!!!");
+		}
+
+		Node current = null;
+
+		Queue<Node> q = new LinkedList<Node>();
+		boolean found = false;
+		q.add(root);
+
+		int size = 0;
+
+		while (!q.isEmpty() && !found) {
+
+			size = q.size();
+			while (size-- > 0) {
+
+				current = q.poll();
+
+				if (current.left == n || current.right == n) {
+					found = true;
+				} else {
+					if (current.left != null) {
+						q.add(current.left);
+					}
+					if (current.right != null) {
+						q.add(current.right);
+					}
+				}
+			}
+		}
+
+		if (q.size() == 0)
+			System.out.println("No Cousins!!!");
+
+		while (!q.isEmpty()) {
+			System.out.print(q.poll().data + " ");
+		}
+
+	}
+
+	private static Node insertBSTRec(Node root, int data) {
+
+		if (root == null) {
+			root = new Node(data);
+			return root;
+		}
+
+		if (data <= root.data) {
+			root.left = insertBSTRec(root.left, data);
+		}
+		if (data > root.data) {
+			root.right = insertBSTRec(root.right, data);
+		}
+
+		return root;
+	}
+
+	private static void insertBST(Node root, int data) {
+
+		Node current = root;
+		Node parent;
+
+		while (true) {
+			parent = current;
+			if (data <= current.data) {
+
+				current = current.left;
+
+				if (current == null) {
+					parent.left = new Node(data);
+					return;
+				}
+
+			} else {
+
+				current = current.right;
+
+				if (current == null) {
+					parent.right = new Node(data);
+					return;
+				}
+			}
+		}
+
+	}
+
+	private static void printInorder(Node root) {
+
+		if (root == null)
+			return;
+
+		printInorder(root.left);
+		System.out.print(root.data + " ");
+		printInorder(root.right);
+	}
+
+	private static boolean isMirror(Node root1, Node root2) {
+
+		if (root1 == null && root2 == null)
+			return true;
+
+		if (root1 == null || root2 == null)
+			return false;
+
+		return (root1.data == root2.data && isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left));
+	}
+
+	private static void printLevelOrder(Node root) {
+
+		int h = height(root);
+
+		for (int i = 1; i <= h; i++) {
+			printLevel(root, i);
+		}
+
+	}
+
+	private static void printLevel(Node root, int level) {
+		if (root == null)
+			return;
+
+		if (level == 1) {
+			System.out.println(root.data);
+		} else if (level > 1) {
+			printLevel(root.left, level - 1);
+			printLevel(root.right, level - 1);
+		}
+
+	}
+
+	private static int height(Node root) {
+
+		if (root == null) {
+			return 0;
+		} else {
+			int l = height(root.left);
+			int r = height(root.right);
+
+			return (Math.max(l, r) + 1);
+		}
+	}
+
+}
--- src/dsproblems/Solution.java
+++ src/dsproblems/Solution.java
@@ -1,7 +1,10 @@
 package dsproblems;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Solution {
 
@@ -11,9 +14,52 @@ public class Solution {
 
 		int[] nums = new int[] { -2, -5 };
 
-		System.out.println(maxSubArray(nums));
+		// System.out.println(maxSubArray(nums));
 
 		// System.out.println("Output: " + longestCommonPrefix(new String[] { "a" }));
+
+		System.out.println("Rotation count :: " + openLock(new String[] {"0201","0101","0102","1212","2002"},"0202"));
+	}
+
+	public static int openLock(String[] deadends, String target) {
+		Set<String> begin = new HashSet<>();
+		Set<String> end = new HashSet<>();
+		Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+		begin.add("0000");
+		end.add(target);
+		int level = 0;
+		while (!begin.isEmpty() && !end.isEmpty()) {
+			
+			System.out.println("===>begin " + begin);
+			System.out.println("===>  end " + end);
+			System.out.println(" " );
+			Set<String> temp = new HashSet<>();
+			for (String s : begin) {
+				if (end.contains(s))
+					return level;
+				if (deads.contains(s))
+					continue;
+				deads.add(s);
+				StringBuilder sb = new StringBuilder(s);
+				for (int i = 0; i < 4; i++) {
+					char c = sb.charAt(i);
+					String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i + 1);
+					String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i + 1);
+					
+					//System.out.println("s1 : " + s1);
+					//System.out.println("s2 : " + s2);
+					
+					if (!deads.contains(s1))
+						temp.add(s1);
+					if (!deads.contains(s2))
+						temp.add(s2);
+				}
+			}
+			level++;
+			begin = end;
+			end = temp;
+		}
+		return -1;
 	}
 
 	private static int canCompleteCircuit(int[] gas, int[] cost) {
